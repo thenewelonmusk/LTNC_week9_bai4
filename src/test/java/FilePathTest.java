f805cb457e42a95e0d5d667f71ded53fc44bafe6
haianh <buianhmonke@gmail.com>
2026-05-07 20:09:51 +0700

    refactored code

--- src/test/java/FilePathTest.java
+++ src/test/java/FilePathTest.java
@@ -1,16 +1,14 @@
 import org.junit.jupiter.api.Test;
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FilePathTest {
     @Test
-    void testWindowsPath() {
-        // Ép buộc dùng dấu \ của Windows
-        String path = "src\\test\\resources\\data.txt";
-        File file = new File(path);
-        
-        // Trên Windows: Hệ điều hành tự hiểu và trả về True
-        // Trên Ubuntu/macOS: Hệ thống tìm file tên là "src\test\resources\data.txt" (coi \ là ký tự thường) -> False
-        assertTrue(file.exists(), "Lỗi: Không tìm thấy file trên hệ điều hành này!");
+    void testPath() {
+        Path path = Paths.get("src", "test", "resources", "data.txt");
+        assertTrue(Files.exists(path));
     }
 }
